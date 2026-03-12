d1c3c3adc1ea25315be975cedbff43b438c49caa
Kimmo Ahola @ Systementor <kimmo.ahola@systementor.se>
2026-03-12 13:48:12 +0100

    added recap file

--- recap.py
+++ recap.py
@@ -1,12 +1,12 @@
 # variabel sparar ett värde till en "behållare"
-x = 14 # = betyder inte lika med utan tilldelning
+x = 14  # = betyder inte lika med utan tilldelning
 # == är lika med
 
-x = "Text" # typomvandling sker automatiskt i Python
+x = "Text"  # typomvandling sker automatiskt i Python
 
-heltal = 1 # heltal
-annat_tal = 2.5 # flyttal
-true = True # boolean
+heltal = 1  # heltal
+annat_tal = 2.5  # flyttal
+true = True  # boolean
 string = "1"
 
 result = heltal + annat_tal + true
@@ -16,7 +16,7 @@ konkatenering = "Kimmo" + " " + "Ahola"
 f_name = "Kimmo"
 f_ename = "Ahola"
 
-f_string = f"{f_name} {f_ename}" # formatted string
+f_string = f"{f_name} {f_ename}"  # formatted string
 # innanför {} är interpolation
 
 
@@ -25,16 +25,12 @@ f_str = f"{2*6/4}"
 filename = input("Ge mig ett filnamn: ")
 
 
-
-
-
-
 name = "Kimmo"
 age = 34
 test = "2"
 
 print(name, age, test)
 
-name, test, age = age, name, test # enkelt sätt att flippa på två variabler
+name, test, age = age, name, test  # enkelt sätt att flippa på två variabler
 # tuple unpacking
 print(name, age, test)
